bf89bbb0a9ac29a86c7c36823847ec2ea7efeeaa
gyertya <gyertya@gmail.com>
2018-08-15 12:49:40 +0200

    new person...

---
+++ CreateClass/CreateClass/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person myPerson = new Person() { Name = "Gyertya"};
+            myPerson.SetBirthDate(1986, 03, 16);
+            myPerson.gender = Person.Gender.MALE;
+            Console.WriteLine(myPerson.GetBirthDate);
+            Console.WriteLine(myPerson.ToString);
+            Console.ReadLine();
+        }
+    }
+    public class Person
+    {
+        public DateTime birthDate;
+
+        public Gender gender;
+
+        public enum Gender
+        {
+            MALE,
+            FEMALE
+        }
+
+        public DateTime GetBirthDate
+        {
+            get { return birthDate; }
+        }
+
+        public void SetBirthDate(int yy, int mm, int dd)
+        {
+            birthDate = new DateTime(yy, mm, dd);
+        }
+        public string Name { get; set; }
+
+        public string ToString => $"Name: {Name}, DOB: {birthDate}, Genre: {gender}";
+    }
+}
