84de3ca14fbe704fe3ff1203bc7fb8edd50d5a5c
gyertya <gyertya@gmail.com>
2018-08-15 20:06:55 +0200

    Ex 3

--- CreateClass/CreateClass/Program.cs
+++ CreateClass/CreateClass/Program.cs
@@ -10,22 +10,29 @@ namespace CreateClass
     {
         static void Main(string[] args)
         {
-            Person myPerson = new Person() { Name = "Gyertya"};
-            myPerson.SetBirthDate(1986, 03, 16);
-            myPerson.gender = Person.Gender.MALE;
-            Console.WriteLine(myPerson.GetBirthDate);
-            Console.WriteLine(myPerson.ToString);
-            Console.ReadLine();
-            Employee myEmployee = new Employee();
-            myEmployee.Name = "Candles";
-            myEmployee.SetBirthDate(88, 1, 1);
-            myEmployee.gender = Person.Gender.MALE;
-            myEmployee.Salary = 100000;
-            myEmployee.Profession = "Junior software developer";
-            myEmployee.roomNumber = 1;
-            Console.WriteLine(myEmployee.ToString);
-            Console.ReadLine();
+            //Person myPerson = new Person() { Name = "Gyertya"};
+            //myPerson.SetBirthDate(1986, 03, 16);
+            //myPerson.gender = Person.Gender.MALE;
+            //Console.WriteLine(myPerson.GetBirthDate);
+            //Console.WriteLine(myPerson.ToString);
+            //Console.ReadLine();
+            //Employee myEmployee = new Employee();
+            //myEmployee.Name = "Candles";
+            //myEmployee.SetBirthDate(88, 1, 1);
+            //myEmployee.gender = Person.Gender.MALE;
+            //myEmployee.Salary = 100000;
+            //myEmployee.Profession = "Junior software developer";
+            //myEmployee.roomNumber = 1;
+            //Console.WriteLine(myEmployee.ToString);
+            //Console.ReadLine();
 
+            Employee Kovacs = new Employee("Géza", DateTime.Now, 1000, "léhűtő");
+            Kovacs.Room = new Room(111);
+            Employee Kovacs2 = (Employee)Kovacs.Clone();
+            Kovacs2.Room.Number = 112;
+            Console.WriteLine(Kovacs.ToString());
+            Console.WriteLine(Kovacs2.ToString());
+            Console.ReadKey();
 
         }
     }
@@ -54,16 +61,42 @@ namespace CreateClass
 
         public new string ToString => $"Name: {Name}, DOB: {birthDate}, Gender: {gender}";
     }
-    class Employee : Room
+    class Employee : Person, ICloneable
     {
         public int Salary;
         public string Profession;
+        public Room Room;
 
-        public new string ToString => $"Name: {Name}, DOB: {birthDate}, Gender: {gender}, Salary: {Salary}, Prof: {Profession}, Room: {roomNumber}";
+        public string ToString()
+        {
+            return $"Name: {Name}, DOB: {birthDate}, Gender: {gender}, Salary: {Salary}, Prof: {Profession}, Room: {Room.Number}";
+        }
+
+        public Employee(string name, DateTime time, int salary, string prof)
+        {
+            Name = name;
+            birthDate = time;
+            Salary = salary;
+            Profession = prof;
+        }
+
+        public object Clone()
+        {
+            Employee newEmployee = (Employee)this.MemberwiseClone();
+            newEmployee.Room = new Room(Room.Number);
+            return newEmployee;
+        }
     }
 
-    class Room : Person
+    class Room
     {
-        public int roomNumber { get; set; }
+        public int Number { get; set; }
+
+        public Room() { }
+
+        public Room(int nr)
+        {
+            Number = nr;
+        }
     }
 }
