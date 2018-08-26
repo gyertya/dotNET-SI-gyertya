05a0cb82b38b7d01a96ae55d20d4d8e4de8e1d0e
gyertya <gyertya@gmail.com>
2018-08-26 14:13:33 +0200

    SequentialCollection first part

---
+++ CreateClass/SequentialCollection/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SequentialCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var MyQueue = new Queue<string>(new[] { "First", "Second", "Third", "Fourth" });
+            //MyQueue.Enqueue("First");
+            //MyQueue.Enqueue("Second");
+            //MyQueue.Enqueue("Third");
+            //MyQueue.Enqueue("Fourth");
+
+            foreach (var item in MyQueue)
+            {
+                Console.WriteLine(item);
+            }
+
+            while (MyQueue.Count > 0)
+            {
+                MyQueue.Dequeue();
+            }
+        }
+    }
+}
